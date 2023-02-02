bacf396d5e3af1b2d91dd13a5c94d2c747346b53
Elyar <elyar.zavary@gmail.com>
2023-02-01 16:10:46 -0800

    the python scripts for converting obj files to binvox files is created

---
+++ ycb_binvox.py
@@ -0,0 +1,39 @@
+import os
+import dataset_download_ycb
+import shutil
+
+# Defining folder for downloading the dataset
+output_directory = os.path.join("models", "binvox_files")
+
+# Creating the output directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+def obj2binvox(input_path, file_name, ext, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    command = "./binvox -d 100 " + input_path + file_name + ext
+    print(command)
+    print("converting" + input_path + "to" + output_path + file_name)
+    if (os.system(command) ==0):
+        shutil.move(input_path + file_name + ".binvox", output_path)
+
+
+# specify the directory containing the folders
+root_dir = "models/ycb/"
+
+# loop through each folder in the directory
+for folder_name in os.listdir(root_dir):
+    folder_path = os.path.join(root_dir, folder_name)
+    for root, dirs,files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".obj"): 
+                file_name, ext = os.path.splitext(file)  
+                output_folder_path = os.path.join(output_directory,folder_name)
+                obj2binvox(folder_path + "/google_64k/", file_name, ext, output_folder_path)
+    if os.path.isdir(output_folder_path):
+        if not os.path.exists(output_folder_path):
+            os.makedirs(output_folder)
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            # do something with file_path
